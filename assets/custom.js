1a02ff7d9dc161dc7058ed82823060006d51773b
shkim1026 <60453195+shkim1026@users.noreply.github.com>
2021-11-22 21:16:10 -0800

    Refactor jquery rule into simple css. Removes comments

--- assets/custom.js
+++ assets/custom.js
@@ -1,6 +1,1 @@
 
-// 'Collections list' on hover > 'shop' link changes color
-$('.collection-container__inner').hover(function(){
-    console.log('hello world');
-    $(this).find('.inner__link-text').toggleClass('hover');
-});
