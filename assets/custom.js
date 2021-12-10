a1c94f804c9c91ca8604a495647f4d498a163c3d
shkim1026 <60453195+shkim1026@users.noreply.github.com>
2021-12-09 16:44:25 -0800

    Adds HTML/CSS/JS for mobile viewports for feature/collections-filter

--- assets/custom.js
+++ assets/custom.js
@@ -1,4 +1,22 @@
+//Shows dropdown box for 'Availability' for desktop
 $(".filter__container").click(function(){
-    console.log("hello");
-    $(this).children().last().toggleClass("display-block");
+    const filter__activeContainer = $(this).children().last();
+    filter__activeContainer.toggleClass("display-block");
+});
+
+//Shows Menu for mobile when 'Filter and Sort' is clicked
+$(".filterAndSortBy__label").click(function(){
+    $(this).next().find(".activeContainer").toggleClass("display-block");
+    $('.filterAndSortBy__backgroundCover').toggleClass("display-block");
+});
+
+// Hides Menu for mobile when 'Back' is clicked
+$(".activeContainer__backContainer").click(function(){
+    $(".activeContainer").toggleClass("display-block");
+    $('.filterAndSortBy__backgroundCover').toggleClass("display-block");
+})
+
+// Shows 'availablity' drop down for mobile
+$(".availability-label").click(function(){
+    $(this).next().toggleClass("display-block");
 });
