357c944d9aa8d5806a8999c07cc96c8d2ca804ba
shkim1026 <60453195+shkim1026@users.noreply.github.com>
2021-12-14 16:12:42 -0800

    Fixes: Changes cursor to pointer for filter. Adds chevron up, down, and mixer png. Adds ability to click outside filter container to close.

--- assets/custom.js
+++ assets/custom.js
@@ -1,22 +1,45 @@
-//Shows dropdown box for 'Availability' for desktop
-$(".filter__container").click(function(){
-    const filter__activeContainer = $(this).children().last();
-    filter__activeContainer.toggleClass("display-block");
+//Shows Dropdown box for 'Availability' for DESKTOP
+$(".filter__label").click(function(){
+    const filterActiveContainer = $(this).parent().find('.filter__activeContainer');
+    filterActiveContainer.toggleClass("display-block");
 });
 
-//Shows Menu for mobile when 'Filter and Sort' is clicked
+//Shows Menu for 'Filter and Sort' for MOBILE
 $(".filterAndSortBy__label").click(function(){
     $(this).next().find(".activeContainer").toggleClass("display-block");
     $('.filterAndSortBy__backgroundCover').toggleClass("display-block");
 });
 
-// Hides Menu for mobile when 'Back' is clicked
+// Hides Menu when 'Back' is clicked for MOBILE
 $(".activeContainer__backContainer").click(function(){
     $(".activeContainer").toggleClass("display-block");
     $('.filterAndSortBy__backgroundCover').toggleClass("display-block");
 })
 
-// Shows 'availablity' drop down for mobile
+// Hides Menu container when user clicks outside of container for DESKTOP and MOBILE
+$(document).mouseup(function(e){
+    // Desktop Filter
+    const container = $(".filter__activeContainer");
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        container.removeClass("display-block");
+    }
+    // Mobile Menu
+    const mobileContainer = $(".filterAndSortBy__activeContainer");
+    if (!mobileContainer.is(e.target) && mobileContainer.has(e.target).length === 0) 
+    {
+        mobileContainer.removeClass("display-block");
+        $(".filterAndSortBy__backgroundCover").removeClass("display-block");
+    }
+});
+
+// Shows 'Availability' sub-menu for MOBILE and adjusts chevron accordingly
 $(".availability-label").click(function(){
     $(this).next().toggleClass("display-block");
+    $(this).find(".chevron").toggleClass("display-none");
+});
+
+// Unchecks all 'availability' filter when RESET is clicked
+$(".filter__reset-label").click(function(){
+    $(".filter__input").prop("checked", false);
 });
